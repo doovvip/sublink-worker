b92547b8b2fc1e2b9f7db33d05652503645639d3
doovvip <77657178+doovvip@users.noreply.github.com>
2026-09-07 13:45:26 +0900

    fix(miyou): export reserved standalone extension entry points

--- miyou-standalone/native/MiYouStandaloneAI.h
+++ miyou-standalone/native/MiYouStandaloneAI.h
@@ -5,13 +5,15 @@ NS_ASSUME_NONNULL_BEGIN
 
 typedef void (^MYSAExtensionHandler)(void);
 
-FOUNDATION_EXPORT NSString *MYSAVersion(void);
-FOUNDATION_EXPORT NSArray<NSString *> *MYSAGetCachedReplies(void);
-FOUNDATION_EXPORT void MYSASetExternalContext(NSString *contact, NSArray<NSString *> *context);
-FOUNDATION_EXPORT void MYSAInvalidateReplies(void);
-FOUNDATION_EXPORT void MYSARequestRefresh(void);
-FOUNDATION_EXPORT void MYSARegisterExtension(NSString *identifier, NSString *title, MYSAExtensionHandler handler);
-FOUNDATION_EXPORT NSArray<NSDictionary *> *MYSARegisteredExtensions(void);
-FOUNDATION_EXPORT BOOL MYSAInvokeExtension(NSString *identifier);
+#define MYSA_EXPORT FOUNDATION_EXPORT __attribute__((visibility("default")))
+
+MYSA_EXPORT NSString *MYSAVersion(void);
+MYSA_EXPORT NSArray<NSString *> *MYSAGetCachedReplies(void);
+MYSA_EXPORT void MYSASetExternalContext(NSString *contact, NSArray<NSString *> *context);
+MYSA_EXPORT void MYSAInvalidateReplies(void);
+MYSA_EXPORT void MYSARequestRefresh(void);
+MYSA_EXPORT void MYSARegisterExtension(NSString *identifier, NSString *title, MYSAExtensionHandler handler);
+MYSA_EXPORT NSArray<NSDictionary *> *MYSARegisteredExtensions(void);
+MYSA_EXPORT BOOL MYSAInvokeExtension(NSString *identifier);
 
 NS_ASSUME_NONNULL_END
